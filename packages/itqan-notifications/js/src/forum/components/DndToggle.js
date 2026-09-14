5391b42c7be7e7d2f1ec7815a53b624f2b496653
atanishka308 <atanishka308@gmail.com>
2026-09-14 14:20:50 +0530

    feat: move DND toggle into notification dropdown controls

--- packages/itqan-notifications/js/src/forum/components/DndToggle.js
+++ packages/itqan-notifications/js/src/forum/components/DndToggle.js
@@ -1,6 +1,9 @@
 import app from 'flarum/forum/app';
 import Component from 'flarum/common/Component';
-import Switch from 'flarum/common/components/Switch';
+import Button from 'flarum/common/components/Button';
+import Tooltip from 'flarum/common/components/Tooltip';
+import extractText from 'flarum/common/utils/extractText';
+import classList from 'flarum/common/utils/classList';
 
 export default class DndToggle extends Component {
   view() {
@@ -10,15 +13,19 @@ export default class DndToggle extends Component {
     const active = !!user.preferences().dndEnabled;
 
     return (
-      <div className="DndToggle">
-        <Switch state={active} onchange={(value) => this.save(value)}>
-          {app.translator.trans('itqan-notifications.forum.dnd_toggle_label')}
-        </Switch>
-      </div>
+      <Tooltip text={extractText(app.translator.trans(`itqan-notifications.forum.dnd.${active ? 'disable' : 'enable'}`))}>
+        <Button
+          className={classList('Button', 'Button--link', { active })}
+          icon={active ? 'fas fa-bell-slash' : 'far fa-bell'}
+          onclick={() => this.toggle()}
+        />
+      </Tooltip>
     );
   }
 
-  save(value) {
-    app.session.user.savePreferences({ dndEnabled: value }).then(() => m.redraw());
+  toggle() {
+    const user = app.session.user;
+    const active = !!user.preferences().dndEnabled;
+    user.savePreferences({ dndEnabled: !active }).then(() => m.redraw());
   }
 }
